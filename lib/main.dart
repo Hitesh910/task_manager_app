4f6badd4332ccdcfd243c32f6f2fe2c902341297
Hitesh <Hiteshkhatri910@gmail.com>
2024-10-17 17:54:50 +0530

    TaskManagerpp

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager_app/utils/app_route.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      routes: app_route,
+    ),
+  );
+}
---
+++ lib/utils/app_route.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager_app/home/view/home_screen.dart';
+
+Map<String,WidgetBuilder> app_route = {
+  "/":(context) => HomeScreen(),
+};
