9f07a12a238f39a9d7c32dfee4663fc6f854f8ba
Hitesh <Hiteshkhatri910@gmail.com>
2024-10-18 17:46:14 +0530

    TaskManagerApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:task_manager_app/utils/app_route.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MaterialApp(
       routes: app_route,
